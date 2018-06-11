e981f14c09515f526a51e8a3a01820b29558f8ea
aliismail <ali.ismail.sc@gmail.com>
2018-06-11 21:18:53 +0300

    float

--- webapp.py
+++ webapp.py
@@ -20,7 +20,7 @@ class Data(db.Model):
     __tablename__="data"
     id=db.Column(db.Integer,primary_key=True)
     name_=db.Column(db.String(120),unique=True)
-    coord_=db.Column(db.Integer)
+    coord_=db.Column(db.String(120))
     source_=db.Column(db.String(120))
     quantities_=db.Column(db.Integer)
 
